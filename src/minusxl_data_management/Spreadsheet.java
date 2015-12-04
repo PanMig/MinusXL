1c3c27e123a6566750cb1cfd43f057605f4fe0a4
Alex Emex <polaralex@gmail.com>
2015-12-04 15:50:04 +0200

    Additions to Data Management
    Trying to create Workbook and Spreadsheet objects

--- src/minusxl_data_management/Spreadsheet.java
+++ src/minusxl_data_management/Spreadsheet.java
@@ -1,5 +1,39 @@
 package minusxl_data_management;
 
-public class Spreadsheet {
+import java.lang.reflect.Array;
 
+import javax.swing.table.AbstractTableModel;
+import javax.swing.table.DefaultTableModel;
+
+public class Spreadsheet extends AbstractTableModel {
+	
+	private String[] columnNames;
+	private Object[][] cells;
+
+	public void Spreadsheet(int rows, int columns){
+		
+		cells = new Cell[rows][columns];
+		
+	}
+	
+	@Override
+	public int getRowCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int getColumnCount() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Object getValueAt(int rowIndex, int columnIndex) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+	
 }
--- src/minusxl_data_management/Workbook.java
+++ src/minusxl_data_management/Workbook.java
@@ -1,10 +1,39 @@
 package minusxl_data_management;
 
+import java.util.ArrayList;
+
 public class Workbook {
+	// The Workbook class is the basis of the Data Management system. It contains a List of Spreadsheets and
+	// all the necessary methods to act on the data.
+	// The GUI takes a Current Workbook object and just displays what it sees, or changes data on it through
+	// the contained methods.
+	
+	private String name="MinusXL_Workbook";
+	// The name of the Workbook. Default name given here. Optionally initialized through the constructor.
+	
+	private ArrayList<Spreadsheet> spreadsheets = new ArrayList<Spreadsheet>();
+	// This is the List of Spreadsheets that makes up the data of the Workbook.
+	
+	public Workbook(String name, int rows, int columns){
+	// The constructor creates a Workbook with a Name, and one attached spreadsheet with x rows and y columns
+			
+		spreadsheets.add(
+				new Spreadsheet(rows, columns)
+				);
+		
+	};
+	
+	
+	public Workbook(int rows, int columns){
+	// This polymorphic constructor doesn't have a name argument, thus uses the Default name
+	// and creates a Workbook with one attached spreadsheet with x rows and y columns
+		
+	};
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		
+		
 	}
 
 }
