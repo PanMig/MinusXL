7f1c03d8cf2c1697ab3712f55a2fbc96d5448046
PanMig <panagiotismigo@gmail.com>
2015-12-08 15:54:02 +0200

    GUI and Charts packages added

---
+++ src/minusXl_charts_managment/LineChart.java
@@ -0,0 +1,16 @@
+package minusXl_charts_managment;
+
+import javax.swing.JFrame;
+
+public class LineChart extends JFrame {
+	
+	private static final long serialVersiIDonU = 1L;
+	
+	public LineChart(String windowTitle,String chartTitle){
+		
+	}
+	
+	public void createDataSet(){
+		
+	}
+}
