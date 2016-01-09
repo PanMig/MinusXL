ff63479e9922cba436302e2bd085591441fabe81
Alex Emex <polaralex@gmail.com>
2016-01-09 18:50:12 +0200

    Details on Functions

--- src/minusxl_data_management/StatisticalFunction.java
+++ src/minusxl_data_management/StatisticalFunction.java
@@ -9,9 +9,6 @@ public abstract class StatisticalFunction extends Function {
 		for (int i = 0; i < inputs.length; i++) {
 			// Checking if the values of the input are NumberCells
 			// (which contain Double values).
-			// For the sake of simplicity, we have made a design choice
-			// to only deal with Double values, which can always be converted
-			// to an Integer, if some need arises:
 			if ((inputs[i] instanceof NumberCell) == false) {
 				System.out.println("Error: The input data are not number values.");
 				return (false);
